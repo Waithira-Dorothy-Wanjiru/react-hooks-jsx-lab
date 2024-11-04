3e438a8847b8523deb0636fc3e9df9a5fc18b8c0
Dorothy <dorothywanjiru32@gmail.com>
2024-11-04 04:04:07 +0300

    first

--- src/components/About.js
+++ src/components/About.js
@@ -1,8 +1,18 @@
 import React from "react";
-import { image } from "../data/data";
+import { image } from "../data/data.js";
 
 function About() {
-  return <div>About</div>;
+  return (
+  <div id ="About">
+    <h2>About Me</h2>
+      <p>
+        I am a passionate web developer with a love for creating dynamic and responsive web applications. 
+        My journey in web development has been both challenging and rewarding, and I am excited to continue 
+        learning and growing in this field.
+      </p>
+      <img src={image} alt="I made this" />
+  </div>
+  );
 }
 
 export default About;
